7a392f5623a551d0ec1f4fbecb9051479552eecf
Javier Hermosilla <jvrhermosilla@gmail.com>
2024-08-03 20:13:07 -0300

    formulario proyecto y tareas listos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import FormularioTarea from "./componentes/FormularioTarea";
+import FormularioProyecto from "./componentes/FormularioProyecto";
 
 function App() {
   //const {primary, secondary } = useTheme("state");
   return (
     <div className="box m-4 p-4" >
         <h1 className="title has-text-centered">Esta va a ser la pagina principal</h1>
-        <FormularioTarea />
+        
+        {/* <FormularioProyecto /> */}
+        <FormularioTarea/>
     </div>
     
   );

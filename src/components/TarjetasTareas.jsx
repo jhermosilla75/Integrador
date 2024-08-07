3427cf41ef24708623de5182750c932ba8e3246a
jhermosilla75 <jvrhermosilla@gmail.com>
2024-08-07 13:49:51 -0300

    poquito hice miercoles a la mañana

--- src/components/TarjetasTareas.jsx
+++ src/components/TarjetasTareas.jsx
@@ -4,7 +4,7 @@ function TarjetasTareas({ tarea, proyectoNombre }) {
       <div className="tarea-content">
         <div className="media">
           <div className="media-content">
-            <p className="title is-5 has-text-link">{tarea.title}</p> {/* Nombre de tarea más pequeño y en azul */}
+            <p className="title is-5 has-text-link">{tarea.title}</p> 
             <p className="subtitle is-6 has-text-grey-light">Proyecto: {proyectoNombre} </p>
           </div>
         </div>
